d7a87a557f8cfdb25019bc86d02b29544e2e1744
237129 <237129@KB37-121-C11>
2025-12-10 11:36:17 +0300

    2

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,11 +1,10 @@
-﻿using ConsoleApp2;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace ConsoleApp3
 {
     class Program
     {
